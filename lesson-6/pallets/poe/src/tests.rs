98213ca4f42bdaace3b011862f8229ad314bd3e0
yammyxing <yammyxing@foxmail.com>
2021-01-07 14:08:05 +0800

    feat: add poe test

---
+++ lesson-6/pallets/poe/src/tests.rs
@@ -0,0 +1,103 @@
+use crate::{Error, mock::*};
+use frame_support::{assert_ok, assert_noop};
+use super::*;
+
+#[test]
+fn create_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+
+		assert_eq!(Proofs::<Test>::get(&claim), (1, frame_system::Module::<Test>::block_number()));
+	})
+}
+
+#[test]
+fn create_claim_failed_when_claim_already_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_noop!(
+			PoeModule::create_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ProofAlreadyExist
+		);
+	})
+}
+
+#[test]
+fn revoke_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_ok!(
+			PoeModule::revoke_claim(Origin::signed(1), claim.clone())
+		);
+	})
+}
+
+#[test]
+fn revoke_claim_failed_when_claim_is_not_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
+
+#[test]
+fn remove_claim_failed_when_not_the_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+		assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(2), claim.clone()),
+			Error::<Test>::NotClaimOwner
+		);
+	})
+}
+
+#[test]
+fn transfer_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+		let dest = 2;
+
+		assert_ok!(PoeModule::transfer_claim(Origin::signed(1), claim.clone(), dest));
+
+		assert_eq!(Proofs::<Test>::get(&claim), (dest, frame_system::Module::<Test>::block_number()));
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_claim_is_not_exist() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let dest = 2;
+
+		assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(1), claim.clone(), dest),
+			Error::<Test>::ClaimNotExist
+		);
+	})
+}
+
+#[test]
+fn transfer_claim_failed_when_not_the_owner() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0, 1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+		let dest = 2;
+
+		assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(2), claim.clone(), dest),
+			Error::<Test>::NotClaimOwner
+		);
+	})
+}
